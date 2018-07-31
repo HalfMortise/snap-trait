3470d1d07eeb1eaee5c597484341bf22008eb915
Half Mortise <HalfMortise@users.noreply.github.com>
2018-07-31 08:43:09 -0600

    Separated child from parent class

---
+++ Electric.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ * This is another child class of the parent Guitar: Electric
+ *
+ */
+
+class Electric extends Guitar {
+   use TuningFork;
+
+   protected $type;
+
+   public function __construct(string $sixString, string $newType) {
+      parent::__construct($newSixString);
+      $this->setType($newType);
+   }
+
+   public function getType(): string {
+      return($this->type);
+   }
+
+   public function setType(string $newType): void {
+      $this->type = $newType;
+   }
+}
